80df4aecc07629458b71db25ac70ff1f6c6d54bf
heromanofe <51365889+heromanofe@users.noreply.github.com>
2020-07-25 16:50:56 +0300

    Update Config.py

--- new_rcon_sandstorm/Config.py
+++ new_rcon_sandstorm/Config.py
@@ -1,26 +1,23 @@
-import Classes, mcrcon_new
-token  = 'DISCORD TOKEN HERE' #here ur token
-main_guy = 0 #NOT IMPORTANT, BUT IF YA ADD DISCORD ID OF A GUY HERE, HE COULD DO ADMIN STUFF WITH BOT
+import Classes, mcrcon_new, valve.rcon
+token  = '' #here ur token
+main_guy = 0
 new_serv= []
 #new_serv.append(Classes.Server_match(ip, server_port(rcon port),password, game, mcr, short_name, full_name,max_score(score for bots to win game), query_port)) --> for every new server, example:
+new_serv.append(Classes.Server_match("127.0.0.1", 27011,"Password", 27018, mcr, "No need, forgot to delete this:D", "No need, forgot to delete this:D",0, 22017))
 mcr =""
-#new_serv.append(Classes.Server_match("192.168.0.101", 27888,"MyRconPassword1337", 27016, mcr, "My servers", "number #2",5, 27000))
 
-
-
-
-#####################################################################################################################################################
 def all_ser():
     global new_serv
     all_servers = []
     for i in new_serv:
         try:
             mcr = mcrcon_new.MCRcon(i.ip, i.password,i.server_port)
+            ok = Classes.Server_match(i.ip, i.server_port, i.password, i.game_port,mcr,i.short_name, i.full_name, i.max_score, i.query)
             mcr.connect()
-        
+            
             all_servers.append(Classes.Server_match(i.ip, i.server_port, i.password, i.game_port,mcr,i.short_name, i.full_name, i.max_score, i.query))
         except: 
                 if len(all_servers) > 0:
                     all_servers.remove(all_servers[len(all_servers)-1])
-                print("ERROR, DIDN'T CONNECTED TO RCON")
+                print("ERROR, DIDN'T CONNECTED TO RCON, server IP: {0}:{1}".format(ok.ip, ok.game_port))
     return all_servers
